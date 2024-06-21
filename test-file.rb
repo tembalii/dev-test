a8f13734b6e1933b463e833ee0a4555bf7516a6f
mbodunwa <mbodunwa@kredete.com>
2024-06-21 08:09:23 +0100

    Update test-file.rb

--- test-file.rb
+++ test-file.rb
@@ -3,5 +3,8 @@ class User < ApplicationRecord
   # Using the serialize method without specifying a serialization format, defaults to YAML
   #ruleid: test-rule-ruby
   serialize :preferences, Hash
+  
+  attr_accessible :signature_html, :user_id, :ticket_permission, :occasional
+
   # Other model methods and validations
 end
